01b409876f74a6fa2eb126f090966f86e7c3fc2a
Jesse CreateThis <jesse@createthis.com>
2017-05-08 12:46:44 -0400

    Initial commit

---
+++ Assets/DoStuff.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoStuff : MonoBehaviour {
+
+    private void Report() {
+        BoxCollider boxCollider = GetComponent<BoxCollider>();
+        Vector3 size = boxCollider.size;
+        Vector3 worldSize = transform.TransformVector(size);
+        Debug.Log("size.x=" + size.x + ",worldSize.x="+worldSize.x);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Report();
+	}
+}
