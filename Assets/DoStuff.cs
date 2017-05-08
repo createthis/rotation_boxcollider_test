f8346d5948ad0bf57b18f4c0e25ddcbacf76f504
Jesse CreateThis <jesse@createthis.com>
2017-05-08 12:57:26 -0400

    lock reset rotation before sampling

--- Assets/DoStuff.cs
+++ Assets/DoStuff.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class DoStuff : MonoBehaviour {
 
     private void Report() {
+        Quaternion oldRotation = transform.rotation;
+        transform.rotation = Quaternion.identity;
         BoxCollider boxCollider = GetComponent<BoxCollider>();
         Vector3 size = boxCollider.size;
         Vector3 worldSize = transform.TransformVector(size);
         Debug.Log("size.x=" + size.x + ",worldSize.x="+worldSize.x);
+        transform.rotation = oldRotation;
     }
 
 	// Use this for initialization
